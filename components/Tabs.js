37a15bbc9740b9cabd9af6be4609e61e00d75a1e
sarah rose <xsarahrosemeow@gmail.com>
2020-09-11 21:39:54 -0400

    got information from axios

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,7 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const getInformation = axios.get(
+  "https://lambda-times-api.herokuapp.com/topics"
+);
